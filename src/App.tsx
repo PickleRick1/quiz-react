50fb455f6da8bede71ff396504037deba57c0856
Olga Bordunova <memofpass21@gmail.com>
2023-02-13 18:59:45 +0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,9 @@ const Result: React.FC<ResultProps> = ({ correct }) => {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали {correct} ответа из 10</h2>
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
       <button onClick={() => window.location.reload()}>
         Попробовать снова
       </button>
